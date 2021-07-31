45f94a0d8be41c21a5ab9b51a08509b2ad0c7009
Darran Lofthouse <darran.lofthouse@jboss.com>
2021-07-31 17:01:22 +0100

    [WFGP-207] Do not add a "security-realm" reference in the initial config.

--- maven-plugin/src/main/java/org/wildfly/galleon/maven/FeatureSpecGeneratorInvoker.java
+++ maven-plugin/src/main/java/org/wildfly/galleon/maven/FeatureSpecGeneratorInvoker.java
@@ -407,7 +407,7 @@ public class FeatureSpecGeneratorInvoker {
             lines.add("</extensions>");
             lines.add("<management>");
             lines.add("<management-interfaces>");
-            lines.add("<http-interface security-realm=\"ManagementRealm\">");
+            lines.add("<http-interface>");
             lines.add("<http-upgrade enabled=\"true\"/>");
             lines.add("<socket interface=\"management\" port=\"${jboss.management.http.port:9990}\"/>");
             lines.add("</http-interface>");
