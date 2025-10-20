b841bbece77b7e0e131d1251f95d38a92be44ff2
Jean-Francois Denise <jfdenise@gmail.com>
2025-10-20 16:23:41 +0200

    DO NOT MERGE INTRODUCE COMPILATION FAILURE

--- galleon-plugins/src/main/java/org/wildfly/galleon/plugin/AbstractArtifactInstaller.java
+++ galleon-plugins/src/main/java/org/wildfly/galleon/plugin/AbstractArtifactInstaller.java
@@ -37,6 +37,7 @@ import org.wildfly.galleon.plugin.WfInstallPlugin.ArtifactResolver;
  *
  * @author jdenise
  */
+FOO
 abstract class AbstractArtifactInstaller implements ShadedModel.Installer {
 
     private final Path generatedMavenRepo;
